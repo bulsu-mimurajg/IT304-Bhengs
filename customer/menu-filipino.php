f3d20ee24f89bc916a36a436d14824884aefd8c0
bulsu-mimurajg <2022100488@ms.bulsu.edu.ph>
2024-12-03 03:30:08 +0800

    feature - customer menu // korean and filipino category

--- customer/menu-filipino.php
+++ customer/menu-filipino.php
@@ -1,142 +1,51 @@
 <?php include('includes/header.php'); ?>
 
-<link rel="stylesheet" href="assets/css/1.css">
+<link rel="stylesheet" href="assets/css/menu.css">
 
-<div class="main">
-
-    <?php include('includes/secondary-navbar.php'); ?>
-
-    <div class="cartside" id="cardside">
-        <h1 class="txt">Add to Cart</h1>
-        <ul class="carthead">
-            <li>Qty</li>
-            <li>Size</li>
-            <li>Product</li>
-            <li>Price</li>
-        </ul>
-        <div id="cartItems">
-        </div>
-        <p id="totalPrice">Total: ₱0.00</p>
-        <button class="btn1">Place order</button>
-    </div>
-
-    <!-- FILIPINO -->
-    <div class="Container">
-        <span class="label">BILAO SPECIALS</span>
-        <div class="menubox">
-            <img src="./assets/img/palabok.jpg" alt="Product Image">
-            <h3>Palabok</h3>
-            <p id="priceDisplay"> ₱280.00 </p>
-            <div class="controls">
-                <div class="controlbox">
-                    <div class="quant">
-                        <div class="btnbox">
-                            <button class="qty-btn" id="decrease">-</button>
-                            <input type="text" class="qty-input" value="1">
-                            <button class="qty-btn" id="increase">+</button>
-                        </div>
+<div class="container-md px-4">
+    <div class="row mt-5 justify-content-center">
+        <div class="col-md-9 mt-5 me-5">
+            <div class="card">
+                <div class="card-header">
+                    <?php include('includes/secondary-navbar.php'); ?>
+                </div>
+                <div class="card-body">
+                    <div class="row row-cols-4 justify-content-center">
+                        <?php
+                        $query = "SELECT p.* FROM product p JOIN product_category pc ON p.CategoryID = pc.CategoryID WHERE pc.CategoryName = 'Filipino';";
+                        $menu = mysqli_query($conn, $query);
+                        if ($menu) {
+                            if (mysqli_num_rows($menu) > 0) {
+                                foreach ($menu as $item) :
+                        ?>
+                                    <div class="col d-flex align-items-stretch mt-5 text-center">
+                                        <div class="card">
+                                            <img src="..<?= $item['ProductImage'] ?>" class="card-img-top img-fluid" height="100" alt="<?= $item['ProductName'] ?>">
+                                            <div class="card-body d-flex flex-column">
+                                                <h5 class="card-title"><?= $item['ProductName'] ?></h5>
+                                                <p class="card-text">₱ <?= $item['Price'] ?></p>
+                                                <a href="#" class="btn btn-danger mt-auto add-to-cart"
+                                                    data-product-id="<?= $item['ProductID'] ?>"
+                                                    data-product-name="<?= $item['ProductName'] ?>"
+                                                    data-product-price="<?= $item['Price'] ?>"
+                                                    data-product-image="..<?= $item['ProductImage'] ?>">Add to Cart</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php
+                                endforeach;
+                            } else {
+                                ?>
+                                <h5>No Product Found.</h5>
+                        <?php
+                            }
+                        }
+                        ?>
                     </div>
                 </div>
             </div>
-            <button class="atcbtn">Add to Cart</button>
-        </div>
-
-        <div class="menubox">
-            <img src="./assets/img/palabok.jpg" alt="Product Image">
-            <h3>Pansit</h3>
-            <p id="priceDisplay"> ₱250.00 </p>
-            <div class="controls">
-                <div class="controlbox">
-                    <div class="quant">
-                        <div class="btnbox">
-                            <button class="qty-btn" id="decrease">-</button>
-                            <input type="text" class="qty-input" value="1">
-                            <button class="qty-btn" id="increase">+</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <button class="atcbtn">Add to Cart</button>
-        </div>
-
-        <div class="menubox">
-            <img src="./assets/img/malabon.jpg" alt="Product Image">
-            <h3>Malabon</h3>
-            <p id="priceDisplay"> ₱270.00 </p>
-            <div class="controls">
-                <div class="controlbox">
-                    <div class="quant">
-                        <div class="btnbox">
-                            <button class="qty-btn" id="decrease">-</button>
-                            <input type="text" class="qty-input" value="1">
-                            <button class="qty-btn" id="increase">+</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <button class="atcbtn">Add to Cart</button>
-        </div>
-    </div>
-
-    <div class="Container">
-        <span class="label">KAKANIN SPEICIALS</span>
-        <div class="menubox">
-            <img src="./assets/img/maja.jpg" alt="Product Image">
-            <h3>Maja</h3>
-            <p id="priceDisplay"> ₱350.00 </p>
-
-            <div class="controls">
-                <div class="controlbox">
-                    <div class="quant">
-                        <div class="btnbox">
-                            <button class="qty-btn" id="decrease">-</button>
-                            <input type="text" class="qty-input" value="1">
-                            <button class="qty-btn" id="increase">+</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <button class="atcbtn">Add to Cart</button>
-        </div>
-
-        <div class="menubox">
-            <img src="./assets/img/sapin.jpg" alt="Product Image">
-            <h3>Sapin-Sapin</h3>
-            <p id="priceDisplay"> ₱350.00 </p>
-            <div class="controls">
-                <div class="controlbox">
-                    <div class="quant">
-                        <div class="btnbox">
-                            <button class="qty-btn" id="decrease">-</button>
-                            <input type="text" class="qty-input" value="1">
-                            <button class="qty-btn" id="increase">+</button>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <button class="atcbtn">Add to Cart</button>
-        </div>
-        <div class="menubox">
-            <img src="./assets/img/pichi.jpg" alt="Product Image">
-            <h3>Pichi-Pichi</h3>
-            <p id="priceDisplay"> ₱300.00 </p>
-            <div class="controls">
-                <div class="controlbox">
-                    <div class="quant">
-                        <div class="btnbox">
-                            <button class="qty-btn" id="decrease">-</button>
-                            <input type="text" class="qty-input" value="1">
-                            <button class="qty-btn" id="increase">+</button>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <button class="atcbtn">Add to Cart</button>
         </div>
     </div>
 </div>
-<script src="assets/js/proj.js"></script>
 
 <?php include('includes/footer.php'); ?>
